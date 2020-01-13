5d2e356bedae5af4891d94c69e98379b82177109
onitwin <onitwin@gmail.com>
2020-01-13 15:04:44 +0000

    add tests to check name and check cohort, and update name and update cohort

--- specs/homework_on_classes_spec.rb
+++ specs/homework_on_classes_spec.rb
@@ -6,13 +6,36 @@ Minitest::Reporters::SpecReporter.new
 
 class TestClasses < MiniTest::Test
 
+  #set up basic details to be used for all tests
   def setup
     @student=Student.new('Joseph', "G17")
   end
 
-  def test_student_details
+  #test student name is obtainable
+  def test_student_name_fetch
     assert_equal("Joseph",@student.get_name)
   end
+
+  #test student cohort is obtainable
+  def test_student_cohort_fetch
+    assert_equal("G17", @student.get_cohort)
+  end
+
+  # check current name then update student name and check result
+  def test_update_student_name
+    assert_equal("Joseph", @student.get_name)
+    @student.set_name("Mary")
+    assert_equal("Mary",@student.get_name)
+  end
+
+  #check current cohort and update student cohort and check results
+  def test_update_set_cohort
+    assert_equal("G17",@student.get_cohort)
+    @student.set_cohort("G25")
+    assert_equal("G25",@student.get_cohort)
+  end
+
+
 
 
 
